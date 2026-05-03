44733ba980d73961cc38dd4d9a15221adcfe02cd
JesseMJ <thefridayinstituteceo@gmail.com>
2026-05-03 17:23:48 -0500

    feat: Add square logo to footer

--- app/src/components/Footer.jsx
+++ app/src/components/Footer.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Footer.css';
+import squareLogo from '../assets/square-logo.png';
 
 export default function Footer() {
   return (
     <footer className="minimal-footer">
       <div className="footer-content">
+        <img src={squareLogo} alt="FLC Icon" className="footer-logo" />
         <p>Serving Southwest Louisiana</p>
         <p className="powered-by">Powered by The Friday Institute</p>
       </div>
